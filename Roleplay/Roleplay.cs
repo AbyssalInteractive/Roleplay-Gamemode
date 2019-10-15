04ee324b8b5d504c7fd628a56ddf8fd1f25ad195
Valentin <valentinlamothe56@gmail.com>
2019-10-15 23:37:01 +0200

    Init roleplay gamemode

---
+++ Roleplay/Roleplay.cs
@@ -0,0 +1,71 @@
+﻿using NovaLife.Server.Gamemode;
+using Mirror;
+using UnityEngine;
+
+public class Roleplay : Gamemode
+{
+    public override void OnGamemodeInit()
+    {
+        base.OnGamemodeInit();
+    }
+
+    public override void OnPlayerCommand(uint playerId, string command)
+    {
+        base.OnPlayerCommand(playerId, command);
+    }
+
+    public override void OnPlayerConnect(uint playerId)
+    {
+        base.OnPlayerConnect(playerId);
+    }
+
+    public override void OnPlayerDisconnect(uint playerId)
+    {
+        base.OnPlayerDisconnect(playerId);
+    }
+
+    public override void OnPlayerEnterVehicle(uint playerId, uint vehicleId)
+    {
+        base.OnPlayerEnterVehicle(playerId, vehicleId);
+    }
+
+    public override void OnPlayerExitVehicle(uint playerId, uint vehicleId)
+    {
+        base.OnPlayerExitVehicle(playerId, vehicleId);
+    }
+
+    public override void OnPlayerKeyDown(KeyCode key, uint playerId)
+    {
+        base.OnPlayerKeyDown(key, playerId);
+    }
+
+    public override void OnPlayerMessage(string message, uint playerId)
+    {
+        base.OnPlayerMessage(message, playerId);
+    }
+
+    public override void OnPlayerSpawn(uint playerId)
+    {
+        base.OnPlayerSpawn(playerId);
+    }
+
+    public override void OnPlayerUpdate(uint playerId)
+    {
+        base.OnPlayerUpdate(playerId);
+    }
+
+    public override void OnServerLoop()
+    {
+        base.OnServerLoop();
+    }
+
+    public override void OnVehicleDeath(uint vehicleId)
+    {
+        base.OnVehicleDeath(vehicleId);
+    }
+
+    public override void OnVehicleSpawn(uint vehicleId)
+    {
+        base.OnVehicleSpawn(vehicleId);
+    }
+}
